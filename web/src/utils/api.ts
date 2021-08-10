8329a34dd85e9acba31f24869be37222549ba3e1
Lucas Deleage <lucas.deleage@gmail.com>
2021-08-10 12:15:24 +0200

    add constant .env

--- web/src/utils/api.ts
+++ web/src/utils/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios'
+import { API_BASE_URL } from './constant'
 
 const instance = axios.create({
-  baseURL: 'http://localhost:3000/api/v1',
+  baseURL: API_BASE_URL,
 })
 
 export default instance
