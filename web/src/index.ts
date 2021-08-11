63bb8977f5afa8d59d83ace8793621491cc0fb8f
Lucas Deleage <lucas.deleage@gmail.com>
2021-08-11 16:42:17 +0200

    add page connexion

--- web/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { AxiosResponse } from 'axios'
-import api from './utils/api'
-
-const greetings = async () => {
-  const res: AxiosResponse = await api.get('/')
-  console.log(res)
-}
-
-greetings()
