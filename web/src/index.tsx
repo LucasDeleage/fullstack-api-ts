58aa0c81e9501786fee141416b0f44048b922360
Lucas Deleage <lucas.deleage@gmail.com>
2021-08-12 11:47:12 +0200

    setup next app + ts + tailwind

--- web/src/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { App } from './app'
-
-const MOUNT_NODE: HTMLElement | null = document.getElementById('app')
-
-ReactDOM.render(<App />, MOUNT_NODE)
